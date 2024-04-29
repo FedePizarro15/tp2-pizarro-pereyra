c2b02cabf8a3b2a161fdea103eadb9ae8bf6be10
Federico Pizarro <fpizarrodalmaso@udesa.edu.ar>
2024-04-29 16:05:46 -0300

    main

--- funciones.py
+++ funciones.py
@@ -30,7 +30,6 @@ def padding(path : str):
 
     return red, green, blue
 
-<<<<<<< HEAD
 def varCanal(canales : list):
     for i in range(2,5):
         for j in range(2,5): # Bucle por cada elemento del canal
@@ -58,51 +57,6 @@ def varCanal(canales : list):
 
                          # Crear 4 arrays de los 4 cuadrantes
                             # Calcular varianza de cada cuadrante(numpy.var)
-=======
-def varCanal(canal : np.ndarray):
-
-    entornos = []
-    varianzas = {}
-
-    for i, fila in enumerate(canal):
-        for j, elemento in enumerate(fila): # Bucle por cada elemento del canal
-    # for i in range(1):
-    #     for j in range(1):
-            entorno5x5 = []
-
-            for y in range(max(0, i-5), min(i+5, np.shape(canal)[1])): 
-               for x in range(max(0, j-5), min(j+5, np.shape(canal)[0])): 
-
-                    entorno5x5.append(canal[y,x]) # Cada elemento del 5x5 lo agrego a 'entorno5x5'
-
-            matrizEntorno = np.array([entorno5x5[0:5], entorno5x5[5:10], entorno5x5[10:15], entorno5x5[15:20], entorno5x5[20:25]]) # Creo el ndarray con las 5 filas de 'entorno5x5'
-            entornos.append(matrizEntorno)
-
-
-    for i, entorno in enumerate(entornos,1): # Tomo los cuadrantes y les calculo las varianzas
-        cuadranteA = entorno[:3, :3]
-        cuadranteB = entorno[:3, 2:6]
-        cuadranteC = entorno[2:6, :3]
-        cuadranteD = entorno[2:6, 2:6] 
-
-        varA = np.var(cuadranteA)
-        varB = np.var(cuadranteB)
-        varC = np.var(cuadranteC)
-        varD = np.var(cuadranteD)
-
-        varianzas[i] = [varA, varB, varC, varD]
-    return varianzas
-
-    # Promedio tomarlo con np.average
-def calcVarianza(red : np.ndarray, green : np.ndarray, blue: np.ndarray):
-    pass
-
-'''
-
-Calcularía este for dentro de cada iteración, no los guardaria en una lista que tenga los entornos, a no ser que despues nos hagan falta.
-
-'''
->>>>>>> 25aa081f5cea9fea64c3b353d2e3eeb9b3e74e04
 
 def kawahara(red : np.ndarray, green : np.ndarray, blue : np.ndarray): # Aplicar filtro kawahara        
     pass
--- main.py
+++ main.py
@@ -5,15 +5,13 @@ from funciones import padding, varCanal
 def main():
     path = 'tp2-pizarro-pereyra/image.png'
     red, green, blue = padding(path)
-<<<<<<< HEAD
+    
     print("Red\n\n", red, "\n\nGreen\n\n", green, "\n\nBlue\n\n", blue)
     
     canales = red, green, blue
 
     varCanal(canales)
     #     print(f"Cuadrante {i} \n {e}")
-=======
->>>>>>> 25aa081f5cea9fea64c3b353d2e3eeb9b3e74e04
 
     imagen = np.stack([red, green, blue], 0) # Junto de nuevo las 3, la dimensión queda (3, 516, 516)
 
