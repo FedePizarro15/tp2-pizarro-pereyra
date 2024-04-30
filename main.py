7d083873072aa75026a6ff8577cd7ebc0760e71d
Agustín Pereyra <apereyra@udesa.edu.ar>
2024-04-30 19:59:14 -0300

    Kuwahara terminado

--- main.py
+++ main.py
@@ -3,17 +3,11 @@ import numpy as np
 from funciones import *
 
 def main():
-    path = 'tp2-pizarro-pereyra/image.png'
-    # red, green, blue = paddingCanal(path)
-
-    # imagen = np.stack([red, green, blue], 0) # Junto de nuevo las 3, la dimensión queda (3, 516, 516)
-
-    # =================================================================
-
-    imagen = matrizParaFiltro(path)
+    path = 'tp2-pizarro-pereyra\image.png'
     
-    kawahara(imagen)
-
+    a = kuwahara2(path)
+    imagen = Image.fromarray(a)
+    imagen.save("imagen_kuwahara.png")
 
 if __name__ == '__main__':
     main()
